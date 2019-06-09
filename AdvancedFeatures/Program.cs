d51c501a3a11a961b11d8696e46241fb961e57bf
Chengyu Sun <csun@calstatela.edu>
2019-06-09 16:46:18 -0700

    Added examples of the Advanced Features lecture

---
+++ AdvancedFeatures/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdvancedFeatures
+{
+    public static class MyExtensions
+    {
+        public static string Substring(this string s, string begin, string end)
+        {
+            var beginIndex = s.ToUpper().IndexOf(begin.ToUpper());
+            var endIndex = s.ToUpper().LastIndexOf(end.ToUpper());
+            return beginIndex >= 0 && endIndex >= 0 ? s.Substring(beginIndex, endIndex - beginIndex + 1) : "";
+        }
+    }
+
+    class Person
+    {
+        public string FirstName;
+        public string LastName;
+
+        public override string ToString()
+        {
+            return FirstName + " " + LastName;
+        }
+    }
+
+    class Program
+    {
+        static Action CreateMethod()
+        {
+            int value = 10;
+            return () => Console.WriteLine(value++);
+        }
+
+        static void Main(string[] args)
+        {
+            // Extension method
+            Console.WriteLine("Amazon".Substring("a", "b"));
+
+            // Closure
+            var a = CreateMethod();
+            var b = CreateMethod();
+            a();
+            a();
+            b();
+            b();
+
+            // List.FindAll()
+            var people = new List<Person>
+            {
+                new Person{ FirstName = "John", LastName = "Doe"},
+                new Person{ FirstName = "Jane", LastName = "Doe"},
+                new Person{ FirstName = "Tom", LastName = "Smith"},
+            };
+
+            var results = people.FindAll(person => person.LastName == "Doe");
+            foreach (var person in results)
+                Console.WriteLine(person);
+        }
+    }
+}
---
+++ EventExample/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace EventExample
+{
+    class MockTextField
+    {
+        private string text = "";
+
+        public void EnterText(string text)
+        {
+            this.text = text;
+            if (EventHandlers1 != null) EventHandlers1(this.text);
+            if (EventHandlers2 != null) EventHandlers2(this.text);
+        }
+
+        // Event Handling Using Delegate
+
+        private Action<string> EventHandlers1;
+        public void RegisterEventHandler(Action<string> eventHandler)
+        {
+            EventHandlers1 += eventHandler;
+        }
+
+        // Event Handling Using Event
+
+        public event Action<string> EventHandlers2;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var textField = new MockTextField();
+            textField.RegisterEventHandler(msg => Console.WriteLine("Handler1: {0}", msg));
+            textField.RegisterEventHandler(msg => Console.WriteLine("Handler2: {0}", msg));
+
+            textField.EventHandlers2 += msg => Console.WriteLine("Handler3: {0}", msg);
+            textField.EventHandlers2 += msg => Console.WriteLine("Handler4: {0}", msg);
+
+            Console.Write("Please enter some text: ");
+            textField.EnterText(Console.ReadLine());
+
+            /* The following line will cause a compliation error
+             * because EventHandlers2 is declared as an event.
+             */
+            // textField.EventHandlers2 = null;
+        }
+    }
+}
